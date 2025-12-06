7568532a4166ef50ce027cb9fc56e0159af07dd3
voldreck <103228799+voldreck@users.noreply.github.com>
2025-12-05 22:20:34 -0600

    added Exit data class

--- src/org/badmotivator/model/entity/Exit.java
+++ src/org/badmotivator/model/entity/Exit.java
@@ -1,4 +1,21 @@
 package org.badmotivator.model.entity;
 
+/**
+ * Exit class - represents the details of a single exit from a location.
+ * Used to define destinations, or why you cannot reach them.
+ */
 public class Exit {
+
+    private final String destinationId;
+    private final String failureMessage;
+
+    /**
+     * @param destinationId - the ID of the next location (or null if impassable, for a generic response).
+     * @param failureMessage - the specific message to give when movement fails (for specific, non-generic reasons)
+     */
+
+    public Exit (String destinationId, String failureMessage) {
+        this.destinationId = destinationId;
+        this.failureMessage = failureMessage;
+    }
 }
